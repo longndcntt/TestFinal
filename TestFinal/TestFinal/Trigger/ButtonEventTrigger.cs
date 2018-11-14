722467c832613c5012ebfdeb3b1892e5c4a23e35
Long Nguyễn Đình <longndcntt@gmail.com>
2018-11-14 14:39:23 +0700

    Update splash screen and app icon

---
+++ TestFinal/TestFinal/Trigger/ButtonEventTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TestFinal.Trigger
+{
+    public class ButtonEventTrigger : TriggerAction<Button>
+    {
+        protected async override void Invoke(Button sender)
+        {
+            if(sender != null)
+            {
+                await sender.ScaleTo(0.95, 50, Easing.CubicOut);
+                await sender.ScaleTo(1, 50, Easing.CubicIn);
+            }
+        }
+    }
+}
